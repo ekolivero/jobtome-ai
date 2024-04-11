6b15684d48f43c7d7f613a0ced3654f29b524148
Erik Olivero <erik.olivero@jobtome.com>
2024-04-11 19:16:52 +0200

    Enable streaming on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { Dashboard } from "@/components/chat-dashboard";
 import { Suspense } from "react";
 
+export const runtime = "edge";
+
 export default function Home() {
   return (
     <Suspense>
