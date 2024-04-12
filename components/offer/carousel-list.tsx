7fe42234cac1ce47847681f2ed5b0322f7590397
Erik Olivero <erik.olivero@jobtome.com>
2024-04-12 09:59:40 +0200

    Add initial changes

--- components/offer/carousel-list.tsx
+++ components/offer/carousel-list.tsx
@@ -12,6 +12,7 @@ import {
 } from "@/components/ui/carousel";
 import Job, { JobProps } from "./job";
 import Autoplay from "embla-carousel-autoplay";
+import { Button } from "@/components/ui/button";
 
 
 export function CarouselList({ j }: { j: JobProps[]}) {
@@ -35,7 +36,7 @@ export function CarouselList({ j }: { j: JobProps[]}) {
 
   return (
     <Carousel
-      className="max-w-xs mx-auto mt-4"
+      className="max-w-xs mx-auto mt-4 shadow-sm rounded-lg bg-white mb-4 md:hidden"
       setApi={setApi}
       plugins={[
         Autoplay({
@@ -50,7 +51,31 @@ export function CarouselList({ j }: { j: JobProps[]}) {
             <Job j={job} />
           </CarouselItem>
         ))}
+        <CarouselItem>
+          <div className="flex flex-col rounded-lg p-6 bg-white md:hidden h-full align-middle justify-center">
+            <h2 className="text-xl font-bold truncate">Wanna see all offers? </h2>
+            <h3 className="text-lg text-gray-500 dark:text-gray-400">
+              Click on the button below
+            </h3>
+            <Button variant={"outline"} className="mt-6 text-xs">
+              See all offers 
+            </Button>
+          </div>
+        </CarouselItem>
       </CarouselContent>
+      <div className="flex justify-center space-x-2 items-center">
+        {Array.from({ length: count }).map((_, index) => (
+          <div
+            key={index}
+            className={`${
+              current === index + 1 ? "w-2 h-2" : "w-1.5 h-1.5"
+            } bg-gray-400 rounded-full ${
+              current === index + 1 ? "bg-gray-800" : ""
+            }`}
+          />
+        ))}
+      </div>
+      <div className="pb-4" />
     </Carousel>
   );
 }
--- components/offer/job.tsx
+++ components/offer/job.tsx
@@ -1,4 +1,7 @@
+import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from "@/components/ui/tooltip"
 import { Button } from "../ui/button";
+import { Share1Icon } from "@radix-ui/react-icons";
+import { HeartIcon, SaveIcon, Share2Icon } from "lucide-react";
 
 export type JobProps = {
   title: string;
@@ -15,14 +18,57 @@ type JobListProps = {
 
 export default function Job({ j }: { j: JobProps} ) {
     return (
-      <div className="flex flex-col bg-white rounded-lg p-6 dark:bg-gray-800 md:hidden">
-        <h2 className="text-xl font-bold">{j.title} </h2>
+      <div className="flex flex-col rounded-lg p-6 bg-white md:hidden h-full align-middle justify-center">
+        <h2 className="text-xl font-bold truncate">{j.title} </h2>
         <h3 className="text-lg text-gray-500 dark:text-gray-400">
           {j.company}
         </h3>
         <p className="text-sm text-gray-500 dark:text-gray-400">{j.geo.city}</p>
-        <Button variant={"outline"} className="mt-4 text-xs">
-          Apply Now
+        <div className="flex flex-row">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger
+              asChild
+              onClick={(e) => {
+                if (navigator.share) {
+                  navigator
+                    .share({
+                      title: "Example Page",
+                      text: "Check out this interesting webpage!",
+                      url: window.location.href,
+                    })
+                    .then(() => console.log("Successful share"))
+                    .catch((error) => console.log("Error sharing:", error));
+                } else {
+                  alert("Your browser does not support the share API.");
+                }
+              }}
+            >
+              <Button size="icon" variant="ghost">
+                <Share2Icon className="size-4" />
+                <span className="sr-only">Share offer</span>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent side="top">Share offer</TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger
+              asChild
+              onClick={(e) => {
+                console.log("Do something");
+              }}
+            >
+              <Button size="icon" variant="ghost">
+                <HeartIcon className="size-4" />
+                <span className="sr-only">Share offer</span>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent side="top">Share offer</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        </div>
+        <Button variant={"outline"} className="mt-1 text-xs">
+          View Job
         </Button>
       </div>
     );
