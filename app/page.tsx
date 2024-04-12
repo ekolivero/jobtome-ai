2033178e62c0f720ccc5176bcab9b83b3177ea05
Erik Olivero <erik.olivero@jobtome.com>
2024-04-12 11:18:59 +0200

    test new changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Dashboard } from "@/components/chat-dashboard";
 import { Suspense } from "react";
 
 export const runtime = "edge";
+export const revalidate = 0;
 
 export default function Home() {
   return (
